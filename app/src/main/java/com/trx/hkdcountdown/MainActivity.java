365e73d77676248aacba883452d7fe2bc2643cc6
tangruixun <tangruixun@gmail.com>
2016-06-08 16:52:13 +0800

    make a copy; need fix!

--- app/src/main/java/com/trx/hkdcountdown/MainActivity.java
+++ app/src/main/java/com/trx/hkdcountdown/MainActivity.java
@@ -1,13 +1,42 @@
 package com.trx.hkdcountdown;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    Long nowTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MySyncTimeTask myTask = new MySyncTimeTask(this);
+        myTask.execute();
+
     }
+
+    public void startTimer (Long untilMillSecond) {
+
+        int sec = Math.floor();untilMillSecond / 1000);
+
+
+
+
+
+        new CountDownTimer(untilMillSecond, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                mTextField.setText("seconds remaining: " + millisUntilFinished / 1000);
+                //here you can have your logic to set text to edittext
+            }
+
+            public void onFinish() {
+                mTextField.setText("done!");
+            }
+
+        }.start();    }
+
 }
---
+++ app/src/main/java/com/trx/hkdcountdown/MySyncTimeTask.java
@@ -0,0 +1,98 @@
+package com.trx.hkdcountdown;
+
+import android.os.AsyncTask;
+import android.os.SystemClock;
+
+import java.lang.ref.WeakReference;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by TRX on 06/08/2016.
+ */
+public class MySyncTimeTask extends AsyncTask <Void, Void, Long> {
+
+    private WeakReference <MainActivity> weakRef;
+    final static String DEADLINE = "Jul 01 00:00:00 GMT+08:30 2047";
+
+
+    public MySyncTimeTask(MainActivity activity) {
+        weakRef = new WeakReference<>(activity);
+    }
+
+    /**
+     * Override this method to perform a computation on a background thread. The
+     * specified parameters are the parameters passed to {@link #execute}
+     * by the caller of this task.
+     * <p/>
+     * This method can call {@link #publishProgress} to publish updates
+     * on the UI thread.
+     *
+     * @param params The parameters of the task.
+     * @return A result, defined by the subclass of this task.
+     * @see #onPreExecute()
+     * @see #onPostExecute
+     * @see #publishProgress
+     */
+    @Override
+    protected Long doInBackground(Void... params) {
+        //long now = SystemClock.;
+        long milliSecondsUntilHKDir = 0;
+        long hkDeadTimeinMillSecond = ConvertDateToMillSec(DEADLINE);
+        long now = 0;
+        long needToChange = 0;
+        SntpClient sntpClient = new SntpClient();
+
+        if (sntpClient.requestTime("time.hko.hk", 30000)) {
+            now = sntpClient.getNtpTime() + SystemClock.elapsedRealtime() - sntpClient.getNtpTimeReference();
+            long localnow = System.currentTimeMillis();
+            if (Math.abs(localnow-now) > 1000*60*60) {
+                needToChange = 1;
+            }
+        }
+
+        if (needToChange == 1) {
+            // local system is wrong
+            milliSecondsUntilHKDir = hkDeadTimeinMillSecond - now;
+        } else {
+            milliSecondsUntilHKDir = hkDeadTimeinMillSecond -  System.currentTimeMillis();
+        }
+
+        //Date time = new Date(now);
+
+        return milliSecondsUntilHKDir;
+    }
+
+    /**
+     * <p>Runs on the UI thread after {@link #doInBackground}. The
+     * specified result is the value returned by {@link #doInBackground}.</p>
+     * <p/>
+     * <p>This method won't be invoked if the task was cancelled.</p>
+     *
+     * @param aLong The result of the operation computed by {@link #doInBackground}.
+     * @see #onPreExecute
+     * @see #doInBackground
+     * @see #onCancelled(Object)
+     */
+    @Override
+    protected void onPostExecute(Long aLong) {
+        super.onPostExecute(aLong);
+        MainActivity mainActivity = weakRef.get();
+        mainActivity.startTimer (aLong);
+    }
+
+
+    static long ConvertDateToMillSec (String givenDateString) {
+        SimpleDateFormat sdf = new SimpleDateFormat("MMM dd HH:mm:ss z yyyy");
+        long timeInMilliseconds = 0;
+        try {
+            Date mDate = sdf.parse(givenDateString);
+            timeInMilliseconds = mDate.getTime();
+            System.out.println("Date in milli :: " + timeInMilliseconds);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return timeInMilliseconds;
+    }
+}
